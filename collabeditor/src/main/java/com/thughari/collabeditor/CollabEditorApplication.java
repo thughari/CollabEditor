0e86e3ae5199938ec59fa4534b0f29186ba2087e
thughari <thughari3@gmail.com>
2025-05-14 12:55:16 +0530

    initial commit

---
+++ collabeditor/src/main/java/com/thughari/collabeditor/CollabEditorApplication.java
@@ -0,0 +1,13 @@
+package com.thughari.collabeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabEditorApplication.class, args);
+	}
+
+}
---
+++ collabeditor/src/test/java/com/thughari/collabeditor/CollabEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thughari.collabeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
